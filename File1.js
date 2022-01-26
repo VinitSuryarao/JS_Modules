460bdc93ef207dc0e291e5a09eab0996eb85c0de
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-01-26 19:21:46 +0530

    Add files via upload

---
+++ File1.js
@@ -0,0 +1,7 @@
+export let {name}="Vinit Suryarao";
+
+export function GetData()
+{
+    console.log("This is GetData Method()");
+}
+
---
+++ File2.js
@@ -0,0 +1,5 @@
+
+import {name, GetData} from "./File1.js";
+
+console.log(name);
+GetData();
